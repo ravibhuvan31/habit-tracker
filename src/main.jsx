dc6e508841dd8f6e3f64392eddb415111aab7c65
algon31 <ravibhuvan31@gmail.com>
2026-01-07 23:30:48 +0530

    using imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
 
+
+// console.log("ENV TEST:", import.meta.env.VITE_TEST);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
